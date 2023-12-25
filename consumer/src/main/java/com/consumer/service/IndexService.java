207cd343ee9d337e35d6bc61724558cb34ae6497
Dimarik1Krasnodar93 <krasnodar93@mail.ru>
2023-12-25 17:50:47 +0300

    kafka consumer - group_id

--- consumer/src/main/java/com/consumer/service/IndexService.java
+++ consumer/src/main/java/com/consumer/service/IndexService.java
@@ -2,6 +2,8 @@ package com.consumer.service;
 
 import org.springframework.kafka.annotation.KafkaListener;
 
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,12 +11,15 @@ public class IndexService {
     public static final String TOPIC_NAME = "exmp_topic";
     public static final String TOPIC_NAME_2 = "exmp_topic_2";
     @KafkaListener(topics = TOPIC_NAME)
-    public void getByTopicExample(String key, String value) {
-        System.out.println(String.format("key %s value %s", key, value));
+    public void getByTopicExample(String key, String value,
+                                  @Header(KafkaHeaders.GROUP_ID) String groupId) {
+        System.out.println(String.format("key %s value %s group %s", key, value, groupId));
     }
 
     @KafkaListener(topics = TOPIC_NAME_2)
-    public void getByTopicExample2(String key, String value) {
-        System.out.println(String.format("topic 2: key %s value %s", key, value));
+    public void getByTopicExample2(String key, String value,
+                                   @Header(KafkaHeaders.GROUP_ID) String groupId) {
+        System.out.println(String.format("topic 2: key %s value %s group %s", key, value,
+                groupId));
     }
 }
